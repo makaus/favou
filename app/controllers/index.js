a913829815782dad906d9b3c182ea70d448a93e5
ibo94 <ibrahim.alkhanov@hotmail.com>
2017-04-03 14:16:46 +0200

    login alert removed

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,31 +1,33 @@
 // open tab group
 if (Ti.Platform.osname == 'android') {
-	$.login.fbProxy = Alloy.Globals.Facebook.createActivityWorker({lifecycleContainer: $.index});
+	$.login.fbProxy = Alloy.Globals.Facebook.createActivityWorker({lifecycleContainer: $.login});
 }
 
 var fb = require('facebook');
  fb.permissions = ['email'];
- fb.setLoginBehavior(fb.LOGIN_BEHAVIOR_NATIVE);
  fb.initialize();
  fb.authorize();
 
 var fb = require('facebook');
-    fb.addEventListener('login', function(e) {
+    fb.addEventListener('login', function(event) {
         // You *will* get this event if loggedIn == false below
         // Make sure to handle all possible cases of this event
-        if (e.success) {
-            alert('login from uid: '+e.uid+', name: '+JSON.parse(e.data).name);
-            label.text = 'Logged In = ' + fb.loggedIn;
-        } else if (e.cancelled) {
+        if (event.success) {
+        	var main = Alloy.createController('main').getView();
+ 			main.open();
+            // alert('login from uid: '+event.uid+', name: '+JSON.parse(event.data).name);
+            // label.text = 'Logged In = ' + fb.loggedIn;
+        } else if (event.cancelled) {
             // user cancelled 
             alert('cancelled');
         } else {
-            alert(e.error);         
+            alert(event.error);         
         }
     });
     fb.addEventListener('logout', function(e) {
         alert('logged out');
-        label.text = 'Logged In = ' + fb.loggedIn;
     });
+
+ 	
 
 $.login.open();
