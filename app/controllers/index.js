ecafe4965ec4578090f4d853f4a69490ce0dc30a
ibo94 <ibrahim.alkhanov@hotmail.com>
2017-04-05 13:05:54 +0200

    hee

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -28,6 +28,6 @@ var fb = require('facebook');
     fb.addEventListener('logout', function(e) {
         alert('logged out');
     });
-	var main = Alloy.createController('createTask').getView();
+	var main = Alloy.createController('main').getView();
 	main.open();
-//$.login.open();
+// $.login.open();
