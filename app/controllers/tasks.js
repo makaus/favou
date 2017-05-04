4e2c22169b04234504f3a95be4d85a1a0ed8745d
ibo94 <ibrahim.alkhanov@hotmail.com>
2017-05-04 23:00:49 +0200

    tilmeldte opgaver

---
+++ app/controllers/tasks.js
@@ -0,0 +1,7 @@
+// Arguments passed into this controller can be accessed via the `$.args` object directly or:
+var args = $.args;
+
+function gotoAdd(e) {
+ 	var createTask = Alloy.createController('createTask').getView();
+ 	createTask.open();
+}
